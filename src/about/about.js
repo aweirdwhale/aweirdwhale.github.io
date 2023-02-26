94e173cfd8d0f35e3b72d78f94c71d29a27df907
aweirdwhale <goussin.olivier@protonmail.com>
2023-02-27 00:33:07 +0100

    passage html/css & vanilla js

---
+++ src/about/about.js
@@ -0,0 +1,53 @@
+var heros = [ 
+    "On est des élèves de spécialité nsi, on crée des projets parce qu'on aime vivre dangereusement (!!!!) et qu'on veut améliorer la vie des autres (quel altruisme !!!) En plus, ça rapporte des 20 facilement en nous faisant passer pour des génies. Notre dernier chef-d'œuvre c'est JDOcopilot, un petit bijou de technologie et d'optimisation. Ah, et Albatross parce qu'on est pas Baudelaire mais on compte avoir la même postérité !",
+    "Olivier, une baleine bizarre mais si sexy... C’est un peu à cause de moi si les projets ou le site sont pas beaux : je suis developpeur front-end.",
+    "Salut, moi c'est Cédric, alias Tidic. C'est moi le gars qui a toujours des galères avec son pc, qui ramène des erreurs ou les créer, venu de Mars ou de Micromégas. Je suis developpeur back-end à mi temps je crois.",
+    "BONJOUR je suis CLAIRE caractéristiques : canard saucisse \n(elle est spéciale)",
+    "Thomas.",
+]
+var images = [
+    "../../assets/favicon.png",
+    "http://jdocopilot.me/pps/28.jpg",
+    "http://jdocopilot.me/pps/29.jpg",
+    "http://jdocopilot.me/pps/4.jpg",
+    "http://jdocopilot.me/pps/5.jpg",
+];
+
+// on affiche par défaut le premier item de la liste heros et images
+var index = 0;
+var hero = heros[index];
+var imageSrc = images[index];
+
+document.getElementById("mainP").innerHTML = hero;
+image = document.getElementById('heroImg');
+image.src = imageSrc;
+
+// on définit une fonction qui va changer le texte et l'image au click de "next" en augmentant l'index de 1
+function next() {
+    index++;
+    if (index >= heros.length) {
+        index = 0;
+    }
+    hero = heros[index];
+    imageSrc = images[index];
+    document.getElementById("mainP").innerHTML = hero;
+    image = document.getElementById('heroImg');
+    image.src = imageSrc;
+}
+
+// on définit une fonction qui va changer le texte et l'image au click de "previous" en diminuant l'index de 1
+function previous() {
+    index--;
+    if (index < 0) {
+        index = heros.length - 1;
+    }
+    hero = heros[index];
+    imageSrc = images[index];
+    document.getElementById("mainP").innerHTML = hero;
+    image = document.getElementById('heroImg');
+    image.src = imageSrc;
+}
+
+//on regarde si les bouttons next ou previous sont cliqués et on appelle la fonction correspondante
+document.getElementsByClassName('next')[0].addEventListener('click', next);
+document.getElementsByClassName('previous')[0].addEventListener("click", previous);
---
+++ src/landing/landing.js
@@ -0,0 +1,42 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,?;.:/!@#$%^&*()_+<>&é~#'{([-|è`_ç^à@)]=}";
+
+const splashes = [
+    "Alan Turing il est grave surcoté",
+    "On est un peu mégalos mais ne vous inquiétez pas",
+    "Kaneki sait faire du bon café !",
+    "He had a vague sense that trees gave birth to dinosaurs",
+    "HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    '/25468254156486464deqsferhjkqgyukqeruirgeuiqofgc',
+]
+
+let splash;
+function randomSplash() {
+    splash = Math.floor(Math.random() * splashes.length)
+}
+
+randomSplash();
+
+
+document.getElementById("splash").innerHTML = splashes[splash];
+
+document.getElementById("title").onmouseover = event => {
+    let iterations = 0;
+      
+        const interval = setInterval(() => {
+          event.target.innerText = event.target.innerText.split("")
+            .map((letter, index) => {
+              if(index < iterations) {
+                return event.target.dataset.value[index];
+              }
+      
+              return letters[Math.floor(Math.random() * 67)]
+            })
+            .join("");
+      
+          if(iterations >= event.target.dataset.value.length){
+            clearInterval(interval);
+          }
+      
+          iterations += 1 / 3;
+        }, 45);
+}
